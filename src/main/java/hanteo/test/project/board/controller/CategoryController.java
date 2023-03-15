9322ac734037769c6243b96eeccf22cc3c9f1f13
HYKIM <ddh963963@naver.com>
2023-03-16 02:01:20 +0900

    Feat : Content-type 추가

--- src/main/java/hanteo/test/project/board/controller/CategoryController.java
+++ src/main/java/hanteo/test/project/board/controller/CategoryController.java
@@ -1,7 +1,9 @@
 package hanteo.test.project.board.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import hanteo.test.project.board.service.CategoryService;
@@ -13,12 +15,12 @@ public class CategoryController {
 
     private final CategoryService categoryService;
 
-    @GetMapping("find-id/{id}")
-    public String findById(@PathVariable Long id) {
-        return categoryService.findById(id);
+    @GetMapping(path = "find-id/{id}", produces = "application/json; charset=utf-8")
+    public ResponseEntity<String> findById(@PathVariable Long id) {
+        return ResponseEntity.ok().body( categoryService.findById(id));
     }
 
-    @GetMapping("find-name/{name}")
+    @GetMapping(path = "find-name/{name}", produces = "application/json; charset=utf-8")
     public String findByName(@PathVariable String name) {
         return categoryService.findByName(name);
     }
